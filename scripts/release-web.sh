8ddfdd1d39b670919afdba85a2ac7860ff1035a1
David Skoland <davidskoland@gmail.com>
2023-10-30 21:30:48 +0100

    Fix release script

--- scripts/release-web.sh
+++ scripts/release-web.sh
@@ -6,3 +6,17 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${SCRIPT_DIR}/..
 
 godot --export-release "Web"
+
+INPUT_FILE=./docs/GodotPilot.html
+
+# Check if the input file exists
+if [ ! -f "${INPUT_FILE}" ]; then
+    echo "Input file not found!"
+    exit 1
+fi
+
+# Define the script to insert
+script_to_insert='<script src="coi-serviceworker.js"></script>'
+
+# Use sed to insert the script after </title>
+sed -i '' -e 's#<\/title>#<\/title>'"$script_to_insert"'#' "${INPUT_FILE}"
